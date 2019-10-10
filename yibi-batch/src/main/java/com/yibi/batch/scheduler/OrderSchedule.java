43a32e924509762ef5cc3ea4ffcc89f4be41dee7
赵赫 <zgsdzczh@163.com>
2019-10-10 12:24:25 +0800

    commit

--- yibi-batch/src/main/java/com/yibi/batch/scheduler/OrderSchedule.java
+++ yibi-batch/src/main/java/com/yibi/batch/scheduler/OrderSchedule.java
@@ -2,6 +2,7 @@ package com.yibi.batch.scheduler;
 
 import com.yibi.batch.biz.OrderBiz;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,21 +14,21 @@ public class OrderSchedule {
 	/**
 	 * 每日价格变动
 	 */
-//	@Scheduled(cron="0 20 15 * * ?")
+	@Scheduled(cron="0 0 1 * * ?")
 	public void changePrice(){
 		orderBiz.changePrice();
 	}
 	/**
 	 * 每日撤销订单
 	 */
-//	@Scheduled(cron="0 20 15 * * ?")
+	@Scheduled(cron="0 0 23 * * ?")
 	public void cancelOrder(){
 		orderBiz.cancelOrder();
 	}
 	/**
 	 * 每日开放交易功能
 	 */
-//	@Scheduled(cron="0 20 15 * * ?")
+	@Scheduled(cron="0 0 8 * * ?")
 	public void open(){
 		orderBiz.open();
 	}
--- yibi-batch/src/main/java/com/yibi/batch/scheduler/StatisticsSchedule.java
+++ yibi-batch/src/main/java/com/yibi/batch/scheduler/StatisticsSchedule.java
@@ -3,6 +3,7 @@ package com.yibi.batch.scheduler;
 import com.yibi.batch.biz.OrderBiz;
 import com.yibi.batch.biz.StatisticsBiz;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,7 +18,7 @@ public class StatisticsSchedule {
 	/**
 	 * 每日统计
 	 */
-//	@Scheduled(cron="0 0 1 * * ?")
+	@Scheduled(cron="0 30 1 * * ?")
 	public void statistiscDay(){
 		statisticsBiz.statistiscDay();
 	}
